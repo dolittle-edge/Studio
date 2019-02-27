0ebecc49259577b87393d86fdb3c244a1b158545
Vidar Aune Westrum <vidar@dolittle.com>
2019-02-27 13:18:17 +0100

    Moved terminal to be a component

--- Source/Web/Features/components/terminal/terminal.js
+++ Source/Web/Features/components/terminal/terminal.js
@@ -2,8 +2,10 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { containerless } from 'aurelia-framework';
 import { Terminal } from 'xterm';
 
+@containerless()
 export class terminal {
 
     constructor() {
