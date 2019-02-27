0fa021c381a852cb0a55577c3442ceb44e001424
Vidar Aune Westrum <vidar@dolittle.com>
2019-02-27 15:47:28 +0100

    Added open/close principles to terminal window

--- Source/Web/Features/nodes/index.js
+++ Source/Web/Features/nodes/index.js
@@ -2,7 +2,16 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { PLATFORM } from 'aurelia-framework';
+import { observable } from 'aurelia-framework';
 export class index {
+  @observable show_terminal = false;
   constructor() {}
+  open_terminal() {
+    this.show_terminal = true;
+    console.log(this.show_terminal);
+  }
+  close_terminal() {
+    this.show_terminal = false;
+    console.log(this.show_terminal);
+  }
 }
--- Source/Web/Features/nodes/remote_overlay.js
+++ Source/Web/Features/nodes/remote_overlay.js
@@ -1,5 +1,13 @@
-import { customElement, containerless } from 'aurelia-framework';
+import { customElement, containerless, bindable } from 'aurelia-framework';
 
 @customElement('remote-overlay')
 @containerless()
-export class remote_overlay {}
+export class remote_overlay {
+  @bindable visible;
+  close() {
+    this.visible = false;
+  }
+  visibleChanged(nv) {
+    console.log(nv);
+  }
+}
