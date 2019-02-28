ca61f7a69da64229b4369315766cce907310ab63
Vidar Aune Westrum <vidar@dolittle.com>
2019-02-28 13:21:11 +0100

    Got terminal back

--- Source/Web/Features/nodes/NodeDetails.js
+++ Source/Web/Features/nodes/NodeDetails.js
@@ -2,7 +2,12 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { observable } from 'aurelia-framework';
 
 export class NodeDetails {
-  
+  @observable show_terminal = false;
+  constructor() {}
+  open_terminal() {
+    this.show_terminal = true;
+  }
 }
--- Source/Web/Features/nodes/index.js
+++ Source/Web/Features/nodes/index.js
@@ -2,21 +2,17 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { observable, PLATFORM } from 'aurelia-framework';
+import { PLATFORM } from 'aurelia-framework';
 export class index {
-  @observable show_terminal = false;
-  constructor() { }
-  open_terminal() {
-    this.show_terminal = true;
-  }
-  close_terminal() {
-    this.show_terminal = false;
-  }
-
   configureRouter(config, router) {
     config.map([
       {
-        route: ['','node/:id'],
+        route: '',
+        name: 'Node',
+        moduleId: PLATFORM.moduleName('nodes/Unselected')
+      },
+      {
+        route: ['node/:id'],
         name: 'Node',
         moduleId: PLATFORM.moduleName('nodes/NodeDetails')
       },
@@ -24,6 +20,7 @@ export class index {
         route: ['add'],
         name: 'AddNode',
         moduleId: PLATFORM.moduleName('nodes/Add')
-      }]);
+      }
+    ]);
   }
 }
