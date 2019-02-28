a30ab35ead37dfaa9315ece62d3251c5dbfb94ac
Einar Ingebrigtsen <einar@dolittle.com>
2019-02-28 12:53:26 +0100

    Moving on to creation

--- Source/Web/Features/nodes/index.js
+++ Source/Web/Features/nodes/index.js
@@ -2,14 +2,28 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { observable } from 'aurelia-framework';
+import { observable, PLATFORM } from 'aurelia-framework';
 export class index {
   @observable show_terminal = false;
-  constructor() {}
+  constructor() { }
   open_terminal() {
     this.show_terminal = true;
   }
   close_terminal() {
     this.show_terminal = false;
+  }
+
+  configureRouter(config, router) {
+    config.map([
+      {
+        route: ['','node/:id'],
+        name: 'Node',
+        moduleId: PLATFORM.moduleName('nodes/NodeDetails')
+      },
+      {
+        route: ['add'],
+        name: 'AddNode',
+        moduleId: PLATFORM.moduleName('nodes/Add')
+      }]);
   }
 }
