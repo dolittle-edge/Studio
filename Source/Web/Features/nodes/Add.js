28fbcd4de162114f86c21050b8a543443db6aa07
Einar Ingebrigtsen <einar@dolittle.com>
2019-02-28 12:59:05 +0100

    Adding a node from the frontend

--- Source/Web/Features/nodes/Add.js
+++ Source/Web/Features/nodes/Add.js
@@ -2,6 +2,28 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-export class add {
+import { CommandCoordinator } from '@dolittle/commands';
+import { Guid } from '@dolittle/core';
+import { inject } from 'aurelia-dependency-injection';
+import { AddNode } from './AddNode';
 
+@inject(CommandCoordinator)
+export class add {
+  #commandCoordinator;
+
+  name = '';
+
+  constructor(commandCoordinator) {
+    this.#commandCoordinator = commandCoordinator;
+  }
+
+
+  async perform() {
+
+    let command = new AddNode();
+    command.id = Guid.create();
+    command.name = this.name;
+
+    let result = await this.#commandCoordinator.handle(command);
+  }
 }
