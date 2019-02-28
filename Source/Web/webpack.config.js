0234e57288d542cf90c70529d40f5a6c836490aa
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-02-28 11:51:33 +0100

    Add deployment build scripts

--- Source/Web/webpack.config.js
+++ Source/Web/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 require('dotenv').config();
 
-process.env.DOLITTLE_WEBPACK_ROOT = path.resolve('../Core');
-process.env.DOLITTLE_WEBPACK_OUT = path.resolve('../Core/wwwroot');
+process.env.DOLITTLE_WEBPACK_ROOT = path.resolve('.');
+process.env.DOLITTLE_WEBPACK_OUT = path.resolve('./dist');
 process.env.DOLITTLE_FEATURES_DIR = path.resolve('./Features');
 process.env.DOLITTLE_COMPONENT_DIR = path.resolve('./Components');
 
