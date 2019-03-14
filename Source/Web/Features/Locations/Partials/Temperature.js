b3fa0d5f72509c754e8d838324da9ab37eaff64f
Vidar Aune Westrum <vidar@dolittle.com>
2019-03-14 11:41:42 +0100

    Splitted code into partials

---
+++ Source/Web/Features/Locations/Partials/Temperature.js
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {customElement, bindable, containerless} from 'aurelia-framework';
+
+@customElement('temperature')
+@containerless()
+export class Temperature {
+
+}
