abd4a6b1a4589dd605363a20d8112be00a599ac0
Vidar Aune Westrum <vidar@dolittle.com>
2019-03-15 10:22:31 +0100

    Moved header to separate component

---
+++ Source/Web/Features/Locations/Partials/HeaderBar.js
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {customElement, containerless, bindable} from 'aurelia-framework';
+
+@customElement('header-bar')
+@containerless()
+export class HeaderBar {
+}
