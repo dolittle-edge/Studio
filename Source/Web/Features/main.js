8ab5c71f7802959a989cc0d99372227e13f8b11c
Vidar Aune Westrum <vidar@dolittle.com>
2019-03-19 15:52:41 +0100

    Added missing license headers

--- Source/Web/Features/main.js
+++ Source/Web/Features/main.js
@@ -1,10 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import environment from './environment';
 import { PLATFORM } from 'aurelia-pal';
 import * as Bluebird from 'bluebird';
 
 import { CommandCoordinator } from '@dolittle/commands';
 import { QueryCoordinator } from '@dolittle/queries';
-
 
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
 Bluebird.config({ warnings: { wForgottenReturn: false } });
