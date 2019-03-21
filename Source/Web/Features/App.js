e9886f0d1ba363a9b2f36356bc222530a1158be8
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-03-21 10:34:41 +0100

    Fix casing

--- Source/Web/Features/App.js
+++ Source/Web/Features/App.js
@@ -10,13 +10,13 @@ export class App {
   configureRouter(config, router) {
     config.options.pushState = true;
     config.map([
-      { route: '', name: 'Home', moduleId: PLATFORM.moduleName('home') },
-      { route: 'applications', name: 'Applications', moduleId: PLATFORM.moduleName('Applications/index') },
-      { route: 'installation', name: 'Installation', moduleId: PLATFORM.moduleName('Installation/index') },
-      { route: 'locations', name: 'Locations', moduleId: PLATFORM.moduleName('Locations/index') },
+      { route: '', name: 'Home', moduleId: PLATFORM.moduleName('Home') },
+      { route: 'applications', name: 'Applications', moduleId: PLATFORM.moduleName('Applications/Index') },
+      { route: 'installation', name: 'Installation', moduleId: PLATFORM.moduleName('Installation/Index') },
+      { route: 'locations', name: 'Locations', moduleId: PLATFORM.moduleName('Locations/Index') },
       { route: 'locations/add-location', name: 'AddLocation', moduleId: PLATFORM.moduleName('Locations/AddLocation') },
       { route: 'locations/add-node', name: 'AddNode', moduleId: PLATFORM.moduleName('Locations/AddNode') },
-      { route: 'setup', name: 'Setup', moduleId: PLATFORM.moduleName('Setup/index') }
+      { route: 'setup', name: 'Setup', moduleId: PLATFORM.moduleName('Setup/Index') }
     ]);
 
     this.router = router;
