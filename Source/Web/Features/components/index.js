990b32d07306f1076d2691275aa3e34fe90341b9
Vidar Aune Westrum <vidar@dolittle.com>
2019-03-27 10:53:20 +0100

    Added an admin list for side bar footer

--- Source/Web/Features/components/index.js
+++ Source/Web/Features/components/index.js
@@ -5,8 +5,11 @@
 import { PLATFORM } from 'aurelia-pal';
 
 export function configure(config) {
+  config.globalResources(PLATFORM.moduleName('./AdminList/AdminList'));
   config.globalResources(PLATFORM.moduleName('./Chart/Chart'));
   config.globalResources(PLATFORM.moduleName('./InlineToolbar/InlineToolbar'));
+  config.globalResources(PLATFORM.moduleName('./NavigationGroup/NavigationGroup'));
+  config.globalResources(PLATFORM.moduleName('./NavigationItem/NavigationItem'));
   config.globalResources(PLATFORM.moduleName('./NavigationList/NavigationList'));
   config.globalResources(PLATFORM.moduleName('./SideBar/SideBar'));
   config.globalResources(PLATFORM.moduleName('./terminal/terminal'));
