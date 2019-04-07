21a7fed0f9162486470fb135f0b847e9c6d60f37
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-07 06:49:58 +0200

    Started looking into specs inside the same progject

---
+++ Source/Domain/Nodes/for_NodeCommandHandlers/when_doing_things.cs
@@ -0,0 +1,11 @@
+using Machine.Specifications;
+
+namespace Domain.Nodes.for_NodeCommandHandlers
+{
+    public class when_doing_things
+    {
+
+        It should_do_stuff = () => true.ShouldBeTrue();
+        
+    }
+}
