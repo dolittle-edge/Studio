4aae1d41585eec7194e3bb1842b5f9b9b2ec3989
Sindre Wilting <sindrewilting@gmail.com>
2019-11-03 14:39:46 +0100

    Start on upgrading edge-studio plugin

---
+++ Source/Tooling/Plugin/internal.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Dolittle. All rights reserved.
+*  Licensed under the MIT License. See LICENSE in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+import './turnOffLogging';
+export * from './CommandGroupsProvider';
+export * from './CommandsProvider';
+export * from './NamespaceProvider';
+
+//edge
+export * from './edge/EdgeNamespace'
+
+// edge/get
+export * from './edge/get/GetCommandGroup';
+export * from './edge/get/GetLocation';
+export * from './edge/get/GetLocations';
+
+// edge/show
+export * from './edge/show/ShowCommandGroup';
+export * from './edge/show/ShowLocations';
+export * from './edge/get/GetLocations';
