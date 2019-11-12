aec3a03e6e495c6d1899239a7edaff28e5010467
Einar Ingebrigtsen <einar@dolittle.com>
2019-11-12 14:31:03 +0100

    Setting development tenant till we have auth

--- Source/API/Telemetry/TelemetryController.cs
+++ Source/API/Telemetry/TelemetryController.cs
@@ -5,6 +5,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Dolittle.Collections;
 using TimeSeries.DataPoints;
+using Dolittle.Execution;
+using Dolittle.Tenancy;
 
 namespace API.Telemetry
 {
@@ -16,18 +18,22 @@ namespace API.Telemetry
     {
         readonly ILocationStatuses _locationStatuses;
         readonly IDataPointMessenger _dataPointMessenger;
+        readonly IExecutionContextManager _executionContextManager;
 
         /// <summary>
         /// Initializes a new instance of <see cref="TelemetryController"/>
         /// </summary>
         /// <param name="locationStatuses"><see cref="ILocationStatuses"/> for dealing with status for locations</param>
+        /// <param name="executionContextManager"></param>
         /// <param name="dataPointMessenger"><see cref="IDataPointMessenger"/> for dealing with </param>
         public TelemetryController(
             ILocationStatuses locationStatuses,
+            IExecutionContextManager executionContextManager,
             IDataPointMessenger dataPointMessenger)
         {
             _locationStatuses = locationStatuses;
             _dataPointMessenger = dataPointMessenger;
+            _executionContextManager = executionContextManager;
         }
 
         /// <summary>
@@ -38,6 +44,7 @@ namespace API.Telemetry
         [HttpPost]
         public ActionResult Post([FromBody] NodeTelemetry nodeTelemetry)
         {
+            _executionContextManager.CurrentFor(TenantId.Development);
             nodeTelemetry.Metrics.ForEach(_ =>
             {
                 _dataPointMessenger.Push(
