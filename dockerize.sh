097e49075966730af02ee9f5507c32d0e578767e
Einar Ingebrigtsen <einar@dolittle.com>
2019-11-22 09:07:42 +0100

    Fixing dockerization

--- dockerize.sh
+++ dockerize.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker build -t dolittle.azurecr.io/edge/core -f Source/Core/Dockerfile .
+export VERSION=$(git tag --sort=-version:refname | head -1)
+docker build --no-cache -f .//Dockerfile -t dolittle/edge-studio . --build-arg CONFIGURATION="Release"
+docker tag dolittle/edge-studio dolittle/edge-studio:$VERSION
+docker push dolittle/edge-studio:latest
+docker push dolittle/edge-studio:$VERSION
