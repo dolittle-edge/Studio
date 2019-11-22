c9412732822bfd1f4059ca5659ff488ee56c2de1
Joel Hoisko <joel.hoisko@gmail.com>
2019-11-22 09:08:49 +0100

    Add preliminary RegisterNodeWithInstallation command to CLI

--- Source/Tooling/Plugin/edge/register/RegisterNodeCommand.ts
+++ Source/Tooling/Plugin/edge/register/RegisterNodeCommand.ts
@@ -7,33 +7,41 @@ import { IDependencyResolvers, PromptDependency, argumentUserInputType, IsNotEmp
 import { ICanOutputMessages, IBusyIndicator } from "@dolittle/tooling.common.utilities";
 import { requireInternet, IConnectionChecker} from "@dolittle/tooling.common.packages";
 import { CommandCoordinator } from "@dolittle/commands";
-import { RegisterNode } from "../../internal";
+import { RegisterNodeWithInstallation } from "../../internal";
 
 const name = 'node';
 const description = 'register a new node with a name';
 
-const registerPromptDependency = new PromptDependency(
-    'name',
-    'The name of the node',
-    [new IsNotEmpty()],
-    argumentUserInputType,
-    'The name of the node'
-);
+const registerPromptDependency = [
+    new PromptDependency(
+        'name',
+        'The name of the node',
+        [new IsNotEmpty()],
+        argumentUserInputType,
+        'The name of the node'),
+    new PromptDependency(
+        'installation name',
+        'The name of the installation',
+        [new IsNotEmpty()],
+        argumentUserInputType,
+        'The name of the installation'),
+];
 
 export class RegisterNodeCommand extends Command {
 
     constructor(private _edgeAPI: string, private _connectionChecker: IConnectionChecker, 
         private _commandCoordinator: CommandCoordinator) {
-        super(name, description, false, undefined, [registerPromptDependency]);
+        super(name, description, false, undefined, registerPromptDependency);
     }
     
     async onAction(commandContext: CommandContext, dependencyResolvers: IDependencyResolvers,
         failedCommandOutputter: IFailedCommandOutputter, outputter: ICanOutputMessages, busyIndicator: IBusyIndicator) {
         let context = await dependencyResolvers.resolve({}, this.dependencies);
-        let name: any = context[registerPromptDependency.name];
+        let nodeName: any = context[registerPromptDependency[0].name];
+        let installationName: any = context[registerPromptDependency[1].name];
         await requireInternet(this._connectionChecker, busyIndicator);
         CommandCoordinator.apiBaseUrl = this._edgeAPI;
-        let commandResult = await this._commandCoordinator.handle(new RegisterNode(name));
+        let commandResult = await this._commandCoordinator.handle(new RegisterNodeWithInstallation(nodeName, installationName));
         outputter.print(commandResult as any);
     }
 }
