09fee212bbac4d4e6ffe5dbf53fb5b63e4ae2da7
Joel Hoisko <joel.hoisko@gmail.com>
2019-11-25 17:23:13 +0100

    Add missing  piece for changing node registering

--- Source/Tooling/Plugin/Installations/RegisterNodeWithInstallation.ts
+++ Source/Tooling/Plugin/Installations/RegisterNodeWithInstallation.ts
@@ -7,14 +7,14 @@ import { Command } from  '@dolittle/commands';
 export class RegisterNodeWithInstallation extends Command
 {
     name: string;
-    siteName: string;
     installationName: string;
+    siteName: string;
 
-    constructor(name: string, siteName: string, installationName: string) {
+    constructor(name: string,installationName: string, siteName: string) {
         super('56442e08-5a8c-443a-8d0b-fd544b597ed6');
 
         this.name = name;
-        this.siteName = siteName;
         this.installationName = installationName;
+        this.siteName = siteName;
     }
 }
