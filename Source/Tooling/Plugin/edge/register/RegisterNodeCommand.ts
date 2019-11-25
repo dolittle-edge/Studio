9be889bcc55aab03b708d7a010b7574f7357df38
Joel Hoisko <joel.hoisko@gmail.com>
2019-11-25 17:22:38 +0100

    Change order of params for node registering

--- Source/Tooling/Plugin/edge/register/RegisterNodeCommand.ts
+++ Source/Tooling/Plugin/edge/register/RegisterNodeCommand.ts
@@ -21,17 +21,17 @@ const registerPromptDependency = [
         argumentUserInputType,
         'The name of the node'),
     new PromptDependency(
-        'site name',
-        'The name of the site',
-        [new IsNotEmpty()],
-        argumentUserInputType,
-        'The name of the site'),
-    new PromptDependency(
         'installation name',
         'The name of the installation',
         [new IsNotEmpty()],
         argumentUserInputType,
         'The name of the installation'),
+    new PromptDependency(
+        'site name',
+        'The name of the site',
+        [new IsNotEmpty()],
+        argumentUserInputType,
+        'The name of the site')
 ];
 
 export class RegisterNodeCommand extends AuthenticatedCommand {
@@ -44,11 +44,11 @@ export class RegisterNodeCommand extends AuthenticatedCommand {
     async onAction(commandContext: CommandContext, dependencyResolvers: IDependencyResolvers, failedCommandOutputter: IFailedCommandOutputter, outputter: ICanOutputMessages, busyIndicator: IBusyIndicator) {
         let context = await dependencyResolvers.resolve({}, this.dependencies);
         let nodeName: any = context[registerPromptDependency[0].name];
-        let siteName: any = context[registerPromptDependency[1].name];
-        let installationName: any = context[registerPromptDependency[2].name];
+        let installationName: any = context[registerPromptDependency[1].name];
+        let siteName: any = context[registerPromptDependency[2].name];
         await requireInternet(this._connectionChecker, busyIndicator);
         CommandCoordinator.apiBaseUrl = this._edgeAPI;
-        let commandResult = await this._commandCoordinator.handle(new RegisterNodeWithInstallation(nodeName, siteName, installationName));
+        let commandResult = await this._commandCoordinator.handle(new RegisterNodeWithInstallation(nodeName, installationName, siteName));
         outputter.print(commandResult as any);
     }
 }
